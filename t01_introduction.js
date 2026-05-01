58c86c80afbff694216e9688571bcd25c67c2c66
oreo <24416bm@hvhs.school.nz>
2026-05-01 02:33:41 +0000

    May 1, 2026, 2:33 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,16 @@
 /****************************
-This is a block comment.
-Put your header comment here!
+Name of task:
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("hello")
 
-// This is a single line comment
+// Variables
+
+/****************************
+Main code
+****************************/
+
+/****************************
+Functions
+****************************/
 
--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,21 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of task:
+****************************/
+console.log("Running t03_maths.js")
+console.log("hello")
+
+// Variables
+let userName = "Beatrix";
+let userAge = "15";
+let userMoney = "25";
+let currentYear = "2026";
+/****************************
+Main code
+****************************/
+console.log("Hi " + userName);
+console.log("As of " + currentYear + " you are " + userAge + " years old.")
+console.log("You have " + userMoney + " dollars.")
+/****************************
+Functions
+****************************/
+
