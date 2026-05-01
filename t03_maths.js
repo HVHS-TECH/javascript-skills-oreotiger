c334370b911a51a7becd4c30b92af227533d2f54
oreo <24416bm@hvhs.school.nz>
2026-05-01 02:50:14 +0000

    May 1, 2026, 2:50 AM

--- t03_maths.js
+++ t03_maths.js
@@ -9,7 +9,7 @@ let userName = "Beatrix";
 let userAge = "15.5";
 let currentYear = "2026";
 let userBirthYear = currentYear - userAge;
-let futureYear = currentYear + 10;
+let futureAge = userAge + 10;
 let userMoney = "25";
 let spentMoney = userMoney / 2;
 let giftMoney = spentMoney + 3;
@@ -20,7 +20,7 @@ Main code
 console.log("Hi " + userName);
 console.log("As of " + currentYear + " you are " + userAge + " years old.")
 console.log("You were born in " + userBirthYear)
-console.log("In 10 years you will be " + futureYear + "years old")
+console.log("In 10 years you will be " + futureAge + "years old")
 console.log("You have " + userMoney + " dollars.")
 console.log("You spent half of your money, now you have " + spentMoney + " dollars")
 console.log("Then you get $3, now you have " + giftMoney + " dollars")
