714614ac03eb2fb2fd7309afb1fa078109f8565e
oreo <24416bm@hvhs.school.nz>
2026-05-06 10:13:50 +0000

    May 6, 2026, 10:13 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,22 +1,27 @@
-console.log("Task 4 - Debugging")
-
-// calculate the area
-var area = length + width;
-
-// calculate the perimeter
-var perimeter = 2 (length) + 2 * width; 
+console.log("Running t04_debugging.js")
+console.log("Hello")
 
 // Set up the size of the page.
+var width = 10;
 var length = 8;
-var width = 10
+
+// calculate the area
+var area = length * width;
+
+// calculate the perimeter
+var perimeter = (2 * length) + (2 * width); 
+
+
+
 
 // Display the results
-console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area)
-console.log("The same piece of paper has a perimeter of "+perimeter)
+console.log("An area of a piece of paper with a length of "+ length + ", and a width of "+ width +" is "+ area)
+console.log("The same piece of paper has a perimeter of "+ perimeter)
 
 // Check, does this result look sensible
-if (perimeter > area){
+if (perimeter < area){
     console.log("Yay, your area is larger, this is right for this example")
-}else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example)
+}
+else {
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
 }
--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,40 @@
-/* Paste your code from task 3, Maths here */
+// JS and HTML link
+const OUTPUT = document.getElementById ("spaceForJavaScriptOutput")
+OUTPUT.innerHTML = "<h3>Added by JavaScript</h3>";
+OUTPUT.innerHTML += "<p>Hi</p> + userName";
+OUTPUT.innerHTML +=
+OUTPUT.innerHTML +=
+OUTPUT.innerHTML +=
+OUTPUT.innerHTML +=
+OUTPUT.innerHTML +=
+
+/****************************
+Name of task:
+****************************/
+console.log("Running t05_JavaScript_and_HTML.js")
+console.log("Hello")
+
+// Variables
+let userName = "Beatrix";
+let userAge = 15.5;
+let currentYear = 2026;
+let userBirthYear = currentYear - userAge;
+
+let userMoney = 25;
+let spentMoney = userMoney / 2;
+let giftMoney = spentMoney + 3;
+
+/****************************
+Main code
+****************************/
+console.log("Hi " + userName);
+console.log("As of " + currentYear + " you are " + userAge + " years old.")
+console.log("You were born in " + userBirthYear)
+console.log("In 10 years you will be " + (userAge + 10) + " years old")
+console.log("You have " + userMoney + " dollars.")
+console.log("You spent half of your money, now you have " + spentMoney + " dollars")
+console.log("Then you get $3, now you have " + giftMoney + " dollars")
+/****************************
+Functions
+****************************/
+
