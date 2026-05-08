23176b49d9c2e657c634abe19dcbc168e07b1f2b
oreo <24416bm@hvhs.school.nz>
2026-05-08 02:42:40 +0000

    May 8, 2026, 2:42 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,60 @@
-/* Paste your code from the last task */
+/****************************
+Name of task:
+****************************/
+console.log("Running t10_input_from_HTML.js")
+console.log("Hello")
+const OUTPUT = document.getElementById ("spaceForJavaScriptOutput")
+
+//Functions
+function welcome(){
+    //Add a line to the HTML page
+    OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+}
+function displayProduct (_name, _price){
+    OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>"
+}
+function start (){
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+// Variables
+let userName = "Beatrix";
+let userAge = 15.5;
+let currentYear = 2026;
+let userBirthYear = currentYear - userAge;
+
+let userMoney = 25;
+let spentMoney = userMoney / 2;
+let giftMoney = spentMoney + 3;
+
+
+
+// JS and HTML link
+function start (){
+OUTPUT.innerHTML = "<h2>Added to JavaScript</h2>"
+OUTPUT.innerHTML = "<h3>Added by JavaScript</h3>";
+OUTPUT.innerHTML += "<p>Hi " + userName + "</p>";
+OUTPUT.innerHTML += "<p>As of " + currentYear + " you are " + userAge + " years old.</p>";
+OUTPUT.innerHTML += "<p>You were born in " + userBirthYear + "</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be " + (userAge + 10) + " years old.</p>";
+OUTPUT.innerHTML += "<p>You have " + userMoney + " dollars.</p>"
+OUTPUT.innerHTML += "<p>You spent half of your money, now you have " + spentMoney + " dollars.</p>"
+OUTPUT.innerHTML += "<p>Then you get $3, now you have " + giftMoney + " dollars.</p>"
+
+/****************************
+Main code
+****************************/
+welcome()
+displayProduct("Chocolate Bar", 4)
+displayProduct("Chips", 3)
+displayProduct("Drink", 2.50)
+console.log("Hi " + userName);
+console.log("As of " + currentYear + " you are " + userAge + " years old.")
+console.log("You were born in " + userBirthYear)
+console.log("In 10 years you will be " + (userAge + 10) + " years old")
+console.log("You have " + userMoney + " dollars.")
+console.log("You spent half of your money, now you have " + spentMoney + " dollars")
+console.log("Then you get $3, now you have " + giftMoney + " dollars")
+}
