fee78cf5bef0e6c1b0e8152eac3f5d5117fc0de6
oreo <24416bm@hvhs.school.nz>
2026-05-08 02:22:11 +0000

    May 8, 2026, 2:22 AM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -15,7 +15,7 @@ function displayProduct (_name, _price){
     OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>"
 }
 function start (){
-    spaceFor
+    spaceForJavaScriptOutput.innerHTML
 }
 // Variables
 let userName = "Beatrix";
