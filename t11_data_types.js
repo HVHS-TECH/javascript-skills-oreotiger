d96f9fb6e72b5fff6b4d02ac1b511e5253e226b1
oreo <24416bm@hvhs.school.nz>
2026-05-11 21:52:24 +0000

    May 11, 2026, 9:52 PM

--- t11_data_types.js
+++ t11_data_types.js
@@ -64,9 +64,4 @@ console.log("In 10 years you will be " + (userAge + 10) + " years old")
 console.log("You have " + userMoney + " dollars.")
 console.log("You spent half of your money, now you have " + spentMoney + " dollars")
 console.log("Then you get $3, now you have " + giftMoney + " dollars")
-function getFormInput(){
-    const NAME_FIELD = document.getElementById("nameField");
-    let userName = NAME_FIELD.value;
-    OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>"
-}
 }
