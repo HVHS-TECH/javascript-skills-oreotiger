eb72bd9004048c49923714a8348878cc1c33937b
oreo <24416bm@hvhs.school.nz>
2026-05-14 01:21:11 +0000

    May 14, 2026, 1:21 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -25,7 +25,6 @@ function start (){
 }
 
 function getFormInput(){
-    const NAME_FIELD = document.getElementById("nameField");
     let userName = NAME_FIELD.value;
     OUTPUT.innerHTML += "<p>Your name is " + userName + "</p>"
 }
