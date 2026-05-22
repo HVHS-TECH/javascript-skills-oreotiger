ebd9c95e175c00f548f76260be72b5f0a6e30fca
oreo <24416bm@hvhs.school.nz>
2026-05-22 02:33:23 +0000

    May 22, 2026, 2:33 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -8,20 +8,19 @@ console.log("Hello")
 const OUTPUT = document.getElementById ("spaceForJavaScriptOutput");
 const NAME_FIELD = document.getElementById("nameField");
 const AGE_FIELD = document.getElementById("ageField");
+const MONEY_FIELD = document.getElementById("moneyField");
 
 // Variables
-let userAge = Number(AGE_FIELD.value);
+let userAge = Number (AGE_FIELD.value);
 let userName = "Beatrix";
 let currentYear = 2026;
 let userBirthYear = currentYear - userAge;
-
-let userMoney = 25;
+let userMoney = Number (MONEY_FIELD.value);
 let spentMoney = userMoney / 2;
 let giftMoney = spentMoney + 3;
 
 //Functions
 function welcome(){
-    //Add a line to the HTML page
     OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
 }
 function displayProduct (_name, _price){
@@ -32,8 +31,10 @@ function start (){
     spaceForJavaScriptOutput.innerHTML
     userName = NAME_FIELD.value
     OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>"
-    userAge = AGE_FIELD.value
+    userAge = Number (AGE_FIELD.value);
     OUTPUT.innerHTML += "<p>Your age is " + userAge + "</p>"
+    olderUserAge = userAge + 10;
+    userMoney = Number (MONEY_FIELD.value);
 
 // JS and HTML link
 
@@ -42,7 +43,7 @@ function start (){
     OUTPUT.innerHTML += "<p>Hi " + userName + "</p>";
     OUTPUT.innerHTML += "<p>As of " + currentYear + " you are " + userAge + " years old.</p>";
     OUTPUT.innerHTML += "<p>You were born in " + userBirthYear + "</p>";
-    OUTPUT.innerHTML += "<p>In 10 years you will be " + (userAge + 10) + " years old.</p>";
+    OUTPUT.innerHTML += "<p>In 10 years you will be " + olderUserAge + " years old.</p>";
     OUTPUT.innerHTML += "<p>You have " + userMoney + " dollars.</p>"
     OUTPUT.innerHTML += "<p>You spent half of your money, now you have " + spentMoney + " dollars.</p>"
     OUTPUT.innerHTML += "<p>Then you get $3, now you have " + giftMoney + " dollars.</p>"
@@ -54,10 +55,10 @@ function start (){
     displayProduct("Chocolate Bar", 4)
     displayProduct("Chips", 3)
     displayProduct("Drink", 2.50)
-    console.log("Hi " + userName);
+    console.log("Hi " + userName)
     console.log("As of " + currentYear + " you are " + userAge + " years old.")
     console.log("You were born in " + userBirthYear)
-    console.log("In 10 years you will be " + (userAge + 10) + " years old")
+    console.log("In 10 years you will be " + olderUserAge + " years old")
     console.log("You have " + userMoney + " dollars.")
     console.log("You spent half of your money, now you have " + spentMoney + " dollars")
     console.log("Then you get $3, now you have " + giftMoney + " dollars")
