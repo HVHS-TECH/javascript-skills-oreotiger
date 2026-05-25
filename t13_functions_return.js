762ccabd520b5f45e891c6476c7b0b8d1c94022a
oreo <24416bm@hvhs.school.nz>
2026-05-25 10:28:25 +0000

    May 25, 2026, 10:28 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,69 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Functions return
+****************************/
+console.log("Running t13_functions_return.js")
+console.log("Hello")
+
+// Constants
+const OUTPUT = document.getElementById ("spaceForJavaScriptOutput");
+const NAME_FIELD = document.getElementById("nameField");
+const AGE_FIELD = document.getElementById("ageField");
+const MONEY_FIELD = document.getElementById("moneyField");
+
+// Variables
+let userAge = Number (AGE_FIELD.value);
+let userName = "Beatrix";
+let currentYear = 2026;
+let userBirthYear = currentYear - userAge;
+let userMoney = Number (MONEY_FIELD.value);
+
+//Functions
+function welcome(){
+    OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+}
+function displayProduct (_name, _price){
+    OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>"
+}
+function start (){
+// Submit button
+    spaceForJavaScriptOutput.innerHTML
+    userName = NAME_FIELD.value
+    OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>"
+    userAge = Number (AGE_FIELD.value);
+    OUTPUT.innerHTML += "<p>Your age is " + userAge + "</p>"
+    olderUserAge = userAge + 10;
+    userMoney = Number (MONEY_FIELD.value);
+    halfUserMoney = userMoney / 2;
+    addUserMoney = halfUserMoney + 3;
+    userBirthYear = currentYear - userAge;
+
+// JS and HTML Output
+    OUTPUT.innerHTML += "<h2>Added to JavaScript</h2>";
+    OUTPUT.innerHTML += "<h3>Added by JavaScript</h3>";
+    welcome()
+    displayProduct("Chocolate Bar", 4)
+    displayProduct("Chips", 3)
+    displayProduct("Drink", 2.50)
+    OUTPUT.innerHTML += "<p>Hi " + userName + "</p>";
+    OUTPUT.innerHTML += "<p>As of " + currentYear + " you are " + userAge + " years old.</p>";
+    OUTPUT.innerHTML += "<p>You were born in " + userBirthYear + "</p>";
+    OUTPUT.innerHTML += "<p>In 10 years you will be " + olderUserAge + " years old.</p>";
+    OUTPUT.innerHTML += "<p>You have " + userMoney + " dollars.</p>"
+    OUTPUT.innerHTML += "<p>You spent half of your money, now you have " + halfUserMoney + " dollars.</p>"
+    OUTPUT.innerHTML += "<p>Then you get $3, now you have " + addUserMoney + " dollars.</p>"
+    if (addUserMoney >= 4) {
+        OUTPUT.innerHTML += "<p>A chocolate bar costs $4. <br> You CAN afford a chocolate bar.</p> "
+    } else {
+        OUTPUT.innerHTML += "<p>A chocolate bar costs $4. <br> Sorry you CAN'T afford a chocolate bar.</p> "
+    }
+
+
+  //Console Output
+    console.log("Hi " + userName)
+    console.log("As of " + currentYear + " you are " + userAge + " years old.")
+    console.log("You were born in " + userBirthYear)
+    console.log("In 10 years you will be " + olderUserAge + " years old")
+    console.log("You have " + userMoney + " dollars.")
+    console.log("You spent half of your money, now you have " + halfUserMoney + " dollars")
+    console.log("Then you get $3, now you have " + addUserMoney + " dollars")
+}
