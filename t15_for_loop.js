fcd8482c73aa1e5897a1893608a4ec539012c295
oreo <24416bm@hvhs.school.nz>
2026-06-01 06:09:28 +0000

    Jun 1, 2026, 6:09 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,16 @@
-/* Paste your code from the last task */
+// Name of task: For Loop
+
+console.log("Running t15_for_loop.js")
+console.log("Hello")
+
+// Constants
+const BOTTLE_FIELD = document.getElementById("bottleField");
+
+// Variables
+
+// Functions
+function sing () {
+    for (let i=15; i>=0; i--) {
+        console.log("Bottles of milk: " + i);
+    }
+}
